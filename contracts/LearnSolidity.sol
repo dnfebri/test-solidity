1a15ccdb1459e93413a00ede225905489b8ba455
mananjabrik <manan.jabrik@gmail.com>
2022-12-08 14:15:47 +0700

    test contract

---
+++ contracts/LearnSolidity.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract LearnSolidity {
+    string name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
