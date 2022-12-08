9652a254a2abf343ff9180f5956abd93debbec67
mananjabrik <manan.jabrik@gmail.com>
2022-12-08 15:38:24 +0700

    test add user

--- contracts/LearnSolidity.sol
+++ contracts/LearnSolidity.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+struct Person {
+    string name;
+    uint age;
+}
+
 contract LearnSolidity {
     string public name;
     uint public age;
+    uint public id;
+
+    mapping(uint => Person) public _person;
 
     constructor(string memory _name, uint _age) {
         name = _name;
@@ -13,5 +21,16 @@ contract LearnSolidity {
     function replaceName(string memory _name, uint _number) public {
         require(_number > 1, "kurang masuk ilmune boss");
         name = _name;
+    }
+
+    function addUser(string memory _name, uint _age) public {
+        require(_age > 17, "belum cukup umur");
+        _person[id].name = _name;
+        _person[id].age = _age;
+        id++;
+    }
+
+    function getUser(uint _id) public view returns (Person memory) {
+        return _person[_id];
     }
 }
--- test/learn-solidity.test.ts
+++ test/learn-solidity.test.ts
@@ -1,33 +1,38 @@
-import {ethers} from 'hardhat'
-import {expect} from 'chai'
-import {LearnSolidity} from '../typechain-types'
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { LearnSolidity } from '../typechain-types';
 
-let learnSolidity: LearnSolidity = null as any
+let learnSolidity: LearnSolidity = null as any;
 
 describe('Test - Sembarang', () => {
-  it('Deploy contract', async() => {
-    const getCotract = await ethers.getContractFactory('LearnSolidity')
-    learnSolidity = await getCotract.deploy('Manto', 20)
-    await learnSolidity.deployed()
+  it('Deploy contract', async () => {
+    const getCotract = await ethers.getContractFactory('LearnSolidity');
+    learnSolidity = await getCotract.deploy('Manto', 20);
+    await learnSolidity.deployed();
     // console.log(learnSolidity.address);
-    
-  })
-  it('Test name must be manto', async() => {
-    const name = await learnSolidity.name()
-    expect('manto').to.equal(name.toLowerCase())
-  })
+  });
+  it('Test name must be manto', async () => {
+    const name = await learnSolidity.name();
+    expect('manto').to.equal(name.toLowerCase());
+  });
   it('Replace name', async () => {
-    const replace = await learnSolidity.replaceName('Sih Jhon', 2)
+    const replace = await learnSolidity.replaceName('Sih Jhon', 2);
     const txReplace = await replace.wait();
-    expect(1).to.eq(txReplace.status)
+    expect(1).to.eq(txReplace.status);
     // console.log("log" ,learnSolidity.replaceName('sopo'));
-  })
-  it("test error", async () =>{
+  });
+  it('test error', async () => {
     const replace = learnSolidity.replaceName('manan', 1);
-    await expect(replace).revertedWith("kurang masuk ilmune boss")
-  })
+    await expect(replace).revertedWith('kurang masuk ilmune boss');
+  });
   it('Test Replace name must be Sih jhon', async () => {
-    const name = await learnSolidity.name()
-    expect('Sih Jhon').to.equal(name)
-  })
-})
+    const name = await learnSolidity.name();
+    expect('Sih Jhon').to.equal(name);
+  });
+
+  // we have two function addUser and getUser
+  // need test function add user, and create 10 user with random name and age
+  // and get all user
+  // please make sure all user catched
+  // and test error make sure all user age > 17;
+});
